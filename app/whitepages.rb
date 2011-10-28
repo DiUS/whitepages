61ce989c192b5b4140313cda68ab78af32250901
Ren Shao <renshao@gmail.com>
2011-10-28 22:17:17 +1100

    added DiUS font, default avatar, polish page

--- app/whitepages.rb
+++ app/whitepages.rb
@@ -4,7 +4,15 @@ require 'sinatra/static_assets'
 require 'haml'
 
 get '/', :provides => 'html' do
-  haml :index
+  contacts = [
+    {:name => 'Ren', :number => '0431 123 456'},
+    {:name => 'Mitko', :number => '0430 123 456'},
+    {:name => 'Stephen', :number => '0430 123 456'},
+    {:name => 'Ken', :number => '0430 123 456'}
+  ]
+  contacts.each_with_index.map {|contact, index| contact[:avatar] = "images/dius#{index % 3 + 1}.png"}
+
+  haml :index, :locals => { :contacts => contacts }
 end
 
 get '/search' do
